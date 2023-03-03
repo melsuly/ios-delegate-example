e7277a8e4151a0c95eaf1b311ff429eebbe58bc9
Nurasyl Melsuly <nurasylmelsuly@gmail.com>
2023-03-03 13:07:48 +0600

    Delegate using simple example

--- Delegate/ViewController.swift
+++ Delegate/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SetCategoryProtocol {
+    
+    @IBOutlet weak var categoryButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func openModalView(_ sender: Any) {
+        let modal = storyboard?.instantiateViewController(withIdentifier: "ModalViewController") as! ModalViewController
+        
+        modal.modalPresentationStyle = .overCurrentContext
+        
+        modal.categoryArray = ["Бакалея", "Cладости", "Табачные изделия", "Весовые"]
+        
+        modal.delegate = self
+        
+        present(modal, animated: true, completion: nil)
+    }
+    
+    func setCategory(_ category: String) {
+        categoryButton.setTitle(category, for: .normal)
+    }
 }
 
